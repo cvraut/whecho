f8b3338f78438a935045b10e043c9555c88c49ea
cvraut <chinmay0raut@gmail.com>
2024-02-11 16:20:03 -0500

    whecho/whecho.py

--- whecho/_send_message.py
+++ whecho/_send_message.py
@@ -3,13 +3,13 @@
 import requests
 from whecho import _config as config
 
-def post_simple(message, url, config=None, debug=False):
+def post_simple(message, url, conf=None, debug=False):
     """A simple version of the whecho post. Only includes username & content."""
-    if not config:
-        config = config.get_config()
+    if not conf:
+        conf = config.get_config()
         if not url:
-            url = config['default_url']
-    data = get_data(config,message,url,debug)
+            url = conf['default_url']
+    data = get_data(conf,message,url,debug)
     if debug:
         print(f"Data: {data}")
     if not url:
@@ -21,8 +21,10 @@ def post_simple(message, url, config=None, debug=False):
         r.raise_for_status()
     except requests.exceptions.HTTPError as err:
         print(err)
+    if debug:
+        return r
 
-def get_data(config,message,url,debug=False):
+def get_data(conf,message,url,debug=False):
     """General get data function for all supported webhooks."""
     url_keyword_map = {"discord.com": get_discord_data,
                        "slack.com": get_slack_data,
@@ -32,36 +34,36 @@ def get_data(config,message,url,debug=False):
         if keyword in url:
             if debug:
                 print(f"Detected {keyword} in URL")
-            return url_keyword_map[keyword](config,message)
+            return url_keyword_map[keyword](conf,message)
     else:
         if debug:
             print(f"Detected unsupported URL: ",end="")
             print(f"{url.split('/')[2]}")
             print("attempting to send to generic webhook with text field in json post body.")
-        return get_default_data(config,message)
+        return get_default_data(conf,message)
 
-def get_discord_data(config,message):
+def get_discord_data(conf,message):
     """Get the data to send to discord."""
-    data = {'username': f"{config['user']}@{config['machine']}", 
+    data = {'username': f"{conf['user']}@{conf['machine']}", 
             'content': message}
     return data
 
-def get_slack_data(config,message):
+def get_slack_data(conf,message):
     """Get the data to send to slack."""
     data = {'text': message}
     return data
 
-def get_teams_data(config,message):
+def get_teams_data(conf,message):
     """Get the data to send to teams."""
     data = {'text': message}
     return data
 
-def get_webex_data(config,message):
+def get_webex_data(conf,message):
     """Get the data to send to webex."""
     data = {'markdown': message}
     return data
 
-def get_default_data(config,message):
+def get_default_data(conf,message):
     """Get the data to send to a generic webhook."""
     data = {'text': message}
     return data
