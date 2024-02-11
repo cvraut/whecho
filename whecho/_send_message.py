0aa3fcfaac763d516b48890044a5dae6bba308e3
cvraut <chinmay0raut@gmail.com>
2024-02-11 13:44:26 -0500

    cleaned up the send_simple

--- whecho/_send_message.py
+++ whecho/_send_message.py
@@ -9,19 +9,7 @@ def post_simple(message, url, config=None, debug=False):
         config = config.get_config()
         if not url:
             url = config['default_url']
-    if "discord.com" in url:
-        if debug:
-            print(f"Detected discord.com in URL")
-        data = get_discord_data(config, message)
-    elif "slack.com" in url:
-        if debug:
-            print(f"Detected slack.com in URL")
-        data = get_slack_data(config, message)
-    else:
-        if debug:
-            print(f"Detected unsupported URL: ",end="")
-            print(f"{url.split('/')[2]}")
-        data = get_default_data(config, message)
+    data = get_data(config,message,url,debug)
     if debug:
         print(f"Data: {data}")
     if not url:
@@ -33,7 +21,25 @@ def post_simple(message, url, config=None, debug=False):
         r.raise_for_status()
     except requests.exceptions.HTTPError as err:
         print(err)
-        
+
+def get_data(config,message,url,debug=False):
+    """General get data function for all supported webhooks."""
+    url_keyword_map = {"discord.com": get_discord_data,
+                       "slack.com": get_slack_data,
+                       "webhook.office.com": get_teams_data,
+                       "webexapis.com": get_webex_data}
+    for keyword in url_keyword_map:
+        if keyword in url:
+            if debug:
+                print(f"Detected {keyword} in URL")
+            return url_keyword_map[keyword](config,message)
+    else:
+        if debug:
+            print(f"Detected unsupported URL: ",end="")
+            print(f"{url.split('/')[2]}")
+            print("attempting to send to generic webhook with text field in json post body.")
+        return get_default_data(config,message)
+
 def get_discord_data(config,message):
     """Get the data to send to discord."""
     data = {'username': f"{config['user']}@{config['machine']}", 
@@ -43,6 +49,16 @@ def get_discord_data(config,message):
 def get_slack_data(config,message):
     """Get the data to send to slack."""
     data = {'text': message}
+    return data
+
+def get_teams_data(config,message):
+    """Get the data to send to teams."""
+    data = {'text': message}
+    return data
+
+def get_webex_data(config,message):
+    """Get the data to send to webex."""
+    data = {'markdown': message}
     return data
 
 def get_default_data(config,message):
