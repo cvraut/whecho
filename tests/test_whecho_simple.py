2e652220207c181569de513e8aacab1c4c81bae8
cvraut <chinmay0raut@gmail.com>
2024-02-11 16:19:30 -0500

    added the python case option

--- tests/test_whecho_simple.py
+++ tests/test_whecho_simple.py
@@ -3,33 +3,42 @@
 import os
 import subprocess
 import re
+from whecho.whecho import whecho_simple
 
-def simple_post(env_var='TEST_URL'):
+def simple_post(env_var='TEST_URL',cli=True):
     # get the test URL
     url = os.environ.get(f'{env_var}', None)
     if not url:
         raise ValueError(f'No test URL passed. Did you set the ${env_var} environment variable?')
-    # use the command line to run whecho and store any errors
-    command = f"whecho -u {url} -m 'This is an automated test message.' --debug"
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    stdout, stderr = process.communicate()
-    
-    # make sure stderr is a binary empty string
-    assert stderr == b''
-    
-    # extract the Response from stdout (should follow pattern \nResponse: <Response [\d+]>\n)
-    response = re.search(r'Response: <Response \[(\d+)\]>', stdout.decode('utf-8'))
-    http_code = int(response.group(1))
+    if cli:
+        # use the command line to run whecho and store any errors
+        command = f"whecho -u {url} -m 'This is an automated test message.' --debug"
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        stdout, stderr = process.communicate()
+        
+        # make sure stderr is a binary empty string
+        assert stderr == b''
+        
+        # extract the Response from stdout (should follow pattern \nResponse: <Response [\d+]>\n)
+        response = re.search(r'Response: <Response \[(\d+)\]>', stdout.decode('utf-8'))
+        http_code = int(response.group(1))
+    else:
+        resp = whecho_simple('This is an automated test message.',url,debug=True)
+        # extract the HTTP code from the response
+        http_code = resp.status_code
     # make sure that the HTTP code is in the 200s
     assert http_code >= 200 and http_code < 300
 
 def test_simple_slack():
+    # test slack url with cli
     simple_post('TEST_SLACK_URL')
 
 def test_simple_webex():
-    simple_post('TEST_WEBEX_URL')
+    # test webex url with python function
+    simple_post('TEST_WEBEX_URL', False)
+    
 
 if __name__ == "__main__":
-    simple_post()
+    simple_post() # only test discord with main function (duplicated in test_auto_machine.py)
     test_simple_slack()
     test_simple_webex()
